b5504719d615c6f61dd1b5b28314185455731cd6
bigdra50 <ryu1998tea@gmail.com>
2020-04-01 12:55:33 +0900

    えっち

---
+++ AreYouH.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class AreYouH 
+{
+    static AreYouH()
+    {
+        if (EditorApplication.timeSinceStartup > 10) return;
+        Debug.Log("Are you H??");
+        var isH = EditorUtility.DisplayDialog("H??", "あなたはえっちですか??", "えっちです!", "えっちじゃないです");
+        if (isH)
+        {
+            Debug.Log("えっち!!");
+            EditorUtility.DisplayDialog("H??", "えっち!", "(///ω///)");
+        }
+        else
+        {
+            Debug.Log("|ω･`)");
+            EditorApplication.Exit(0);
+        }
+    }
+}
+#endif
